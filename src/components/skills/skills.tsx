aad58830bbeb01c3f94d14c689295d8db7bc830c
xlazure <lukasz.cielak@gmail.com>
2023-02-23 23:47:40 +0100

    fix main.yml

---
+++ src/components/skills/skills.tsx
@@ -0,0 +1,3 @@
+export default function ShowMySkills() {
+  return <div>TEST</div>;
+}
--- src/modules/skillsSection/index.tsx
+++ src/modules/skillsSection/index.tsx
@@ -1,5 +1,10 @@
+import ShowMySkills from "@/components/skills/skills";
 import style from "./skillsSection.module.scss";
 
 export default function SkillsSection() {
-  return <div id="skillsSection" className={style.container}></div>;
+  return (
+    <div id="skillsSection" className={style.container}>
+      <ShowMySkills />
+    </div>
+  );
 }
